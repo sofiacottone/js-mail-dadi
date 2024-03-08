7fea1f8a2ecc845cc82111d7759c4605db25a3c0
sofiacottone <cottonesofia@outlook.it>
2024-03-08 14:24:13 +0100

    init project

---
+++ mail/js/script.js
@@ -0,0 +1,5 @@
+// Mail
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+// Per la soluzione di questo esercizio NON è consentito utilizzare .includes di js!
